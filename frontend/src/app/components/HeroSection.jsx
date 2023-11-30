b27ba6d7e69d23a275e5a24502b4b5c6b95a6468
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2023-11-30 06:34:01 +0000

    updated herosection picture

--- frontend/src/app/components/HeroSection.jsx
+++ frontend/src/app/components/HeroSection.jsx
@@ -77,8 +77,8 @@ const Herosection = () => {
         >
           <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              // src="/images/developer-pic.png"
-              src="/images/about.jpg"
+              src="/images/developer-pic.png"
+              // src="/images/about.jpg"
               // src="/images/web-developer-img.ico"
               alt="web developer image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
