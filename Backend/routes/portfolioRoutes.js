05719c5b005b0c258a02a5cc4cba33edd1948f24
ManiKanta9578 <manikanta9578@gmail.com>
2023-11-30 11:22:21 +0530

    deleted backend files

--- Backend/routes/portfolioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getPortfolioItems,createPortfolioItem} = require('../controllers/portfolioController');
-
-router.get('/portfolio', getPortfolioItems);
-router.post('/portfolio', createPortfolioItem);
-
-module.exports = router;
